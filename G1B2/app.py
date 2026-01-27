23f7dc8d87fea6eeffa1473560b1f9db07d49831
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-01-27 10:57:36 +0530

    routing

---
+++ G1B2/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template    # redirect ,url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+# @app.route('/')
+# def redirect_example():
+#     return redirect(url_for('index'))
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
