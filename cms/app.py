cef943e4b50ff7fe4cd16b159e25b86d77b40928
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-03-02 00:37:44 +0530

    Docker file deploymenty

--- cms/app.py
+++ cms/app.py
@@ -1,18 +1,46 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import MySQLdb.cursors
 
 app = Flask(__name__)
-app.secret_key = 'my_secret_key_12345'  # Better secret key 
+app.secret_key = os.environ.get('SECRET_KEY', 'my_secret_key_12345')
 
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Tanishh#123'
-app.config['MYSQL_DB'] = 'g1b2'
+# Database Configuration from environment variables
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'Tanishh#123')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'g1b2')
 
 mysql = MySQL(app)
+
+# Flask-Login giva central management  
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+# User Model give methods like is auth , is active , is anonymous etc
+class User(UserMixin):
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+
+@login_manager.user_loader
+def load_user(user_id):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    user_data = cursor.fetchone()
+    cursor.close()
+    
+    if user_data:
+        return User(user_data['id'], user_data['username'], user_data['email'], user_data['password'])
+    return None
+
 
 @app.route('/')
 def index():
@@ -20,22 +48,24 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+        
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
-        user = cursor.fetchone()
+        user_data = cursor.fetchone()
         
-        if user and check_password_hash(user['password'], password):
-            session['loggedin'] = True
-            session['id'] = user['id']
-            session['username'] = user['username']
+        if user_data and check_password_hash(user_data['password'], password):
+            user = User(user_data['id'], user_data['username'], user_data['email'], user_data['password'])
+            login_user(user)
+            flash('Login successful!', 'success')
             return redirect(url_for('dashboard'))
         else:
-            flash('Invalid username or password!')
-    
+            flash('Invalid username or password!', 'error')
     
     return render_template('login.html')
 
@@ -61,16 +91,15 @@ def signup():
     return render_template('signup.html')
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    if 'loggedin' in session:
-        return render_template('dashboard.html', username=session['username'])
-    return redirect(url_for('login'))
-
-
+    return render_template('dashboard.html', username=current_user.username)
 
 @app.route('/logout')
+@login_required
 def logout():
-    session.clear()
+    logout_user()
+    flash('You have been logged out.', 'info')
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
